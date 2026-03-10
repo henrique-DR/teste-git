7c71aea3629572af035e40d3d496a22a841d0928
Henrique <henrique.romero@estudante.iftm.edu.br>
2026-03-10 09:34:55 -0300

    Entrada aceitar apenas INT

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-n = int(input('Digite um número: '))
-print(n)
+# n = int(input('Digite um número: '))
+# ('Digte um número!')
+# print(n + 1)
+try:
+    n = int(input('Digite um número: '))
+    print(n + 1)
+except ValueError: 
+    print('Tem que ser um número!')
